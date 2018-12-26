37f0a3b4c18b71da1976e9175466f08d6e56e393
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2018-12-27 00:18:11 +0900

    buefyの導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [],
+  plugins: ['~plugins/buefy'],
 
   /*
   ** Nuxt.js modules
@@ -44,7 +44,8 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma'
+    '@nuxtjs/bulma',
+    'nuxt-buefy'
   ],
 
   /*
