2407b103cf641356c3fedba9473a9a79c80205dd
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2019-01-04 08:14:17 +0900

    一旦commit

---
+++ app/components/students/organisms/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <navbar
+    :list="list"
+    color="info">
+    <NavMenu
+      :list="list"
+      @logout="doLogout"/>
+  </navbar>
+</template>
+
+<script>
+import Navbar from '~/components/common/atoms/Navbar'
+import NavMenu from '~/components/common/molecules/NavMenu'
+export default {
+  name: 'Header',
+  components: { NavMenu, Navbar },
+  data() {
+    return {
+      list: [
+        { label: 'ホーム', link: '/students' },
+        { label: 'テスト', link: '/students/test' }
+      ]
+    }
+  },
+  methods: {
+    doLogout() {
+      this.$emit('logout')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
