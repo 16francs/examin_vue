0e322606b306a9d36a719f9a5b97ba92eb7c456b
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2019-01-11 23:22:57 +0900

    Panelのテスト

---
+++ spec/components/students/atoms/Panel.spec.js
@@ -0,0 +1,21 @@
+import Vuex from 'vuex'
+import Buefy from 'buefy'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Panel from '~/components/students/atoms/Panel'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(Buefy)
+
+describe('components/students/atoms/Panel', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = mount(Panel, { localVue })
+  })
+  describe('template', () => {
+    test('panelが存在するか', () => {
+      const div = wrapper.findAll('div').at(0)
+      expect(div.classes()).toContain('panel')
+    })
+  })
+})
