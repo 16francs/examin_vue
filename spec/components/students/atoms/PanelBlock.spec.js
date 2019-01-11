52972f0c29c7d9b940725ce5733511fe68380d5a
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2019-01-12 00:49:58 +0900

    PanelBlockのテスト

---
+++ spec/components/students/atoms/PanelBlock.spec.js
@@ -0,0 +1,62 @@
+import Vuex from 'vuex'
+import Buefy from 'buefy'
+import { mount, createLocalVue } from '@vue/test-utils'
+import PanelBlock from '~/components/students/atoms/PanelBlock'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(Buefy)
+
+describe('components/students/atoms/PanelBlock', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = mount(PanelBlock, { localVue })
+  })
+  describe('template', () => {
+    test('PanelBlockが存在するか', () => {
+      const div = wrapper.findAll('a').at(0)
+      expect(div.classes()).toContain('panel-block')
+    })
+  })
+  describe('script', () => {
+    describe('props', () => {
+      describe('index', () => {
+        test('indexの初期値', () => {
+          expect(wrapper.props().index).toBe(1)
+        })
+        test('indexに代入', () => {
+          wrapper.setProps({ index: 2 })
+          expect(wrapper.props().index).toBe(2)
+        })
+      })
+      describe('text', () => {
+        test('textの初期値', () => {
+          expect(wrapper.props().text).toBe('')
+        })
+        test('textに代入', () => {
+          wrapper.setProps({ text: '変更テスト' })
+          expect(wrapper.props().text).toBe('変更テスト')
+        })
+      })
+    })
+    describe('methods', () => {
+      describe('select', () => {
+        let mock
+        beforeEach(() => {
+          mock = jest.fn()
+        })
+        test('正常に呼び出されること', () => {
+          wrapper.setMethods({ select: mock })
+          wrapper.vm.select()
+          expect(mock).toBeCalled()
+        })
+
+        test('emitが実行されること', async done => {
+          await wrapper.vm.select()
+          expect(wrapper.emitted().select).toBeTruthy()
+          done()
+        })
+      })
+    })
+  })
+})
