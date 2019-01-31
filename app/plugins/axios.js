10c78d36dd25634c26f4db942d0941799e8b78a6
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-01-31 20:36:23 +0900

    認証処理の編集

--- app/plugins/axios.js
+++ app/plugins/axios.js
@@ -1,10 +1,15 @@
 export default function({ $axios, store }) {
   $axios.onRequest(config => {
+    // API の ホスト名
     config.baseURL = process.env.baseUrl + '/api'
 
-    if (store.state.accessToken) {
-      config.headers.common['access-token'] = store.getters['accessToken']
+    // 認証情報の取得
+    const accessToken = store.getters['accessToken']
+    if (accessToken) {
+      const token = 'Bearer ' + accessToken
+      config.headers.common['Authorization'] = token
     }
+
     return config
   })
 }
