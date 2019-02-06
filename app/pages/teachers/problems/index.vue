9a368a84a83483ff180777996991c2e4305d7d18
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-02-07 00:41:55 +0900

    問題集一覧画面の実装

---
+++ app/pages/teachers/problems/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="section">
+    <teachers-problems />
+  </div>
+</template>
+
+<script>
+import TeachersProblems from '~/components/teachers/templates/TeachersProblems'
+
+export default {
+  layout: 'teachers/default',
+
+  components: {
+    TeachersProblems
+  },
+
+  async asyncData({ store }) {
+    await store.dispatch('teachers/problems/getProblems')
+  }
+}
+</script>
