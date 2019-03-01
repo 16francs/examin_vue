adeec8841ea931f62d090b47a47b626a97ed85db
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-03-01 19:09:17 +0900

    問題一覧ページへのリンクを追加

---
+++ app/components/teachers/templates/TeachersQuestions.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <span>問題一覧コンポーネント</span>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ app/pages/teachers/problems/_id/questions/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section">
+    <teachers-questions />
+  </div>
+</template>
+
+<script>
+import TeachersQuestions from '~/components/teachers/templates/TeachersQuestions'
+
+export default {
+  layout: 'teachers/default',
+
+  components: {
+    TeachersQuestions
+  }
+}
+</script>
