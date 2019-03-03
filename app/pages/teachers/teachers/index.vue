14d87f15d1707fe0b2c0f6b7a8341ed6949e0e07
nishi <a151417k@st.u-gakugei.ac.jp>
2019-03-03 16:07:02 +0900

    Update index.vue

--- app/pages/teachers/teachers/index.vue
+++ app/pages/teachers/teachers/index.vue
@@ -14,11 +14,7 @@ export default {
   },
 
   async asyncData({ store }) {
-    await store.dispatch('teachers/teachers/getTeachers').catch(response => {
-      if (response.data.status === 401) {
-        this.$emit('logout')
-      }
-    })
+    await store.dispatch('teachers/teachers/getTeachers')
   }
 }
 </script>
