1cc7c1dac8635c43d984d5873f96fc3480ba8be8
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2019-03-03 12:33:10 +0900

    生徒情報編集画面の作成

---
+++ app/pages/students/setting.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hero-body">
+    <students-setting-card/>
+  </div>
+</template>
+
+<script>
+import StudentsSettingCard from '../../components/students/molecules/StudentsSettingCard'
+export default {
+  components: { StudentsSettingCard },
+  layout: 'students/default'
+}
+</script>
