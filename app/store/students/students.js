9a90d6738bc90ba1c564b56b3c9ed245da56cb09
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2019-04-26 20:21:30 +0900

    studentを保持するstateを作成

--- app/store/students/students.js
+++ app/store/students/students.js
@@ -1,7 +1,22 @@
-export const state = () => ({})
+export const state = () => ({
+  student: []
+})
 
-export const getters = {}
+export const getters = {
+  student: state => state.student
+}
 
-export const mutations = {}
+export const mutations = {
+  setStudent(state, { student }) {
+    state.student = student
+  }
+}
 
-export const action = {}
+export const actions = {
+  async getStudent({ commit }) {
+    const json = await this.$axios.get('/users/me').catch(() => {
+      throw new Error('Server Error')
+    })
+    commit('setStudent', { student: json.data })
+  }
+}
