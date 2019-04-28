d00b4b9ad94048c16e204127858070cda3402d3a
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2019-04-29 07:52:20 +0900

    pagesから処理を分離

--- app/pages/students/setting.vue
+++ app/pages/students/setting.vue
@@ -1,61 +1,17 @@
 <template>
-  <div class="hero-body">
-    <students-setting-card
-      v-if="!isEditing"
-      :name="student.name"
-      :school="student.school"
-      :login_id="student.login_id"
-      @click="showEditForm"
-    />
-    <student-setting-edit-form
-      v-if="isEditing"
-      @edit="edit"
-      @cancel="cancel"
-    />
-  </div>
+  <students-user-edit />
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-import StudentsSettingCard from '~/components/students/molecules/StudentsSettingCard'
-import StudentSettingEditForm from '~/components/students/organisms/StudentSettingEditForm'
+import StudentsUserEdit from '~/components/students/templates/StudentsUserEdit'
 
 export default {
-  components: { StudentSettingEditForm, StudentsSettingCard },
+  components: { StudentsUserEdit },
   layout: 'students/default',
   // 生徒情報を取得する
   async asyncData({ store }) {
     await store.dispatch('students/students/getStudent').catch(() => {
       console.log('status:', '401')
-    })
-  },
-  data() {
-    return {
-      isEditing: false
-    }
-  },
-  computed: {
-    ...mapGetters({
-      student: 'students/students/student'
-    })
-  },
-  methods: {
-    showEditForm() {
-      this.isEditing = true
-    },
-    edit() {
-      this.update()
-      this.isEditing = false
-      this.$toast.open({
-        message: 'ユーザー情報を編集しました.',
-        type: 'is-success'
-      })
-    },
-    cancel() {
-      this.isEditing = false
-    },
-    ...mapActions({
-      update: 'students/students/getStudent'
     })
   }
 }
